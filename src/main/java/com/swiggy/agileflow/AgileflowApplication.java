f03071190f8eba5d03b746db139fbd78c9929d35
niraj080-er <niranjan888k@gmail.com>
2026-06-09 19:36:48 +0530

    Initial commit

---
+++ src/main/java/com/swiggy/agileflow/AgileflowApplication.java
@@ -0,0 +1,13 @@
+package com.swiggy.agileflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileflowApplication.class, args);
+	}
+
+}
